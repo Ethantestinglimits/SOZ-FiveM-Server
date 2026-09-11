320eb78ff95f81c1f3d5e979b6eb0c6f483cb2bd
Ethan <bcrethan@gmail.com>
2026-09-11 20:26:43 +0200

    fix(vehicle): cap /perf suspension upgrade at level 1

--- resources/[soz]/soz-core/src/client/vehicle/vehicle.custom.provider.ts
+++ resources/[soz]/soz-core/src/client/vehicle/vehicle.custom.provider.ts
@@ -252,7 +252,7 @@ export class VehicleCustomProvider {
                 engine: GetNumVehicleMods(vehicleEntityId, VehicleModType.Engine) - 1,
                 brakes: GetNumVehicleMods(vehicleEntityId, VehicleModType.Brakes) - 1,
                 transmission: GetNumVehicleMods(vehicleEntityId, VehicleModType.Transmission) - 1,
-                suspension: GetNumVehicleMods(vehicleEntityId, VehicleModType.Suspension) - 1,
+                suspension: 1,
                 armor: GetNumVehicleMods(vehicleEntityId, VehicleModType.Armor) - 1,
             },
         };
