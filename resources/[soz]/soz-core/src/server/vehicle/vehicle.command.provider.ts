81300e3048c3bd54a0f9d5851222b8d2a0ad541f
Ethan <bcrethan@gmail.com>
2026-09-11 02:44:17 +0200

    feat(vehicle): add /repair admin command

--- resources/[soz]/soz-core/src/server/vehicle/vehicle.command.provider.ts
+++ resources/[soz]/soz-core/src/server/vehicle/vehicle.command.provider.ts
@@ -59,4 +59,22 @@ export class VehicleCommandProvider {
             oilLevel: newlevel,
         });
     }
+
+    @Command('repair', { role: ['admin'], description: 'Repair closest vehicle (Admin Only)' })
+    async repairCommand(source: number) {
+        const closestVehicle = await this.vehicleSpawner.getClosestVehicle(source);
+
+        this.vehicleStateService.updateVehicleCondition(closestVehicle.vehicleNetworkId, {
+            engineHealth: 1000,
+            bodyHealth: 1000,
+            tankHealth: 1000,
+            dirtLevel: 0,
+            tireHealth: {},
+            tireBurstState: {},
+            tireBurstCompletely: {},
+            tireTemporaryRepairDistance: {},
+            doorStatus: {},
+            windowStatus: {},
+        });
+    }
 }
